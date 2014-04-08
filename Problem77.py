5850e2211feae0dfb8a995500dc36a11f3d2f412
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-04-08 09:48:46 -0700

    Fixing the long broken Problem77

--- Problem77.py
+++ Problem77.py
@@ -17,81 +17,20 @@ Notes on problem 77():
 '''
 #from projectEuler import primes
 
-from PE_primes import isPrime, primesUpTo, factorize
-
-def numberOfWays(n,checker,max):
-	count = 0
-	if n <= 3:
-		return 0
-	primeList = checker[0:max]
-	primeList.reverse()
-	for p in primeList:
-		if isPrime(n-p) and n-p <= p:
-			count += 1
-		count += numberOfWays(n-p,checker,p)
-	return count
-
-def problem77():
-	checker = list(primesUpTo(5000))
-	record = 0
-	for i in range(1,100):
-		r = numberOfWays(i,checker,i)
-		if r >= 5000:
-			return(i)
-
-from itertools import dropwhile
-def problem77a():
-	from PE_primes import primesUpTo
-	primeList = list(primesUpTo(30000))
-	record = 0
-	def numberOfWaysa(n,maximum,_d={(3,3):1,(3,2):0,(2,2):1}):
-		if (n,maximum) in _d:
-			return _d[(n,maximum)]
-		count = 0
-		if isPrime(n) and isPrime(maximum):
-			count += 1
-		for p in dropwhile(lambda x: x>=maximum or n-x <=0, primeList[::-1]):
-			#if n -p <= 0: continue
-			#if isPrime(n-p) and n-p <= p:
-			#	print(n,p)
-			#	count += 1
-			count += numberOfWaysa(n-p,p)
-		_d[(n,maximum)] = count
-		return count
-	print("**",numberOfWaysa(71,71))
-	
-	for i in range(1,100):
-		r = numberOfWaysa(i,i)
-		if r >= 110:
-			return i
-
-from memoize import memoize
-@memoize
-def sopf(n):
-	if n == 1:
-		return 1
-	return sum(set(factorize(n)))
-
-@memoize
-def kappa(n):
-	if n == 0:
-		return 1
-	if n == 1:
-		return 0
-	total = sum(sopf(j)*kappa(n-j) for j in range(1,n+1) )
-	return total/n
-
+from PE_primes import primesUpTo
 from itertools import count
+
 def problem77():
-	print(kappa(11))
-	
-	for n in count(1):
-		print(n,kappa(n))
-		if kappa(n) > 5000:
-			return n
+    primes = primesUpTo(100)
+    for target in count(11):
+        ways = [1] + [0] * target
+        for prime in primes:
+            for j in range(prime, target + 1):
+                ways[j] += ways[j - prime]
+        if ways[target] > 5000:
+            return target
 
 from cProfile import run
 if __name__ == "__main__":
-	#run("problem77()")
-	print(problem77())
- 
+    run("problem77()")
+    print(problem77() == 71)
