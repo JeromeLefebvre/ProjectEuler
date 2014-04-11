c77c6b28a402e42a46c238129828fc312101cdeb
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-04-11 11:36:07 -0700

    Correct solution to Problem207

--- Problem207.py
+++ Problem207.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3.3
+#!/usr/local/bin/python
 
 '''
 http://projecteuler.net/problem=207
@@ -12,76 +12,79 @@ Notes on problem 207():
 
 from itertools import count
 from math import log
-from memoize import memoize
-@memoize
-def P(m):
-	countLog = 0
-	powersOf2 = [2**i for i in range(1,20)]
-	for n in count(3):
-		if n in powersOf2: continue
-		t = log(n,2)
-		if 4**t - 2**t > m:
-			break
-		countLog += 1
 
-	countInt = 0
-	for t in count(1):
-		if 4**t - 2**t > m:
-			break
-		countInt += 1
-	return (countInt, countInt+countLog)
+from quadratics import solveIntegerQuadratic
+
+PRECISION = 10
 
 
-def problem207():
-	for n in count(147630):
-		a,b = P(n)
-		if n in [5,10,15,20,25,30,180,185]:
-			print(n,P(n))
-		if a/b < 1/1234:
-			return n
-	
-
-#Implement this: https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+def positiveSolution(a, b, c):
+    return [s for s in solveIntegerQuadratic(a, b, c) if s > 0]
 
 from decimal import *
+getcontext().prec = PRECISION
+
+
 def f(k):
-	radical = Decimal(1) + Decimal(4)*k
-	radical = Decimal(1) + radical.sqrt()
-	logant /= Decimal(2)
-	return a.ln()/Decimal(2).ln()
+    radical = Decimal(1) + Decimal(4) * k
+    radical = Decimal(1) + radical.sqrt()
+    radical /= Decimal(2)
+    return radical.ln() / Decimal(2).ln()
 
+
+def isInteger(a):
+    return int(a) == a.quantize(Decimal('0.0000001'))
+
+
+def P(n):
+    perfect = 0
+    notperfect = 0
+    k = Decimal(1)
+    while True:
+        t = f(k)
+        if isInteger(2 ** t):
+            if isInteger(t):
+                perfect += 1
+            else:
+                notperfect += 1
+        if k >= n:
+            return perfect, (perfect + notperfect)
+        k += 1
+
+
+def ispowerOf2(x):
+    return x in {2 ** n for n in range(1, 100)}
+
+
+def P(frac):
+    perfect = 0
+    notperfect = 0
+    l = 1
+    while True:
+        k = l*(l+1)    
+        try:
+            x = positiveSolution(1, -1, -k)[0]
+            if ispowerOf2(x):
+                perfect += 1
+            else:
+                notperfect += 1
+        except:
+            pass
+        if perfect / (perfect + notperfect) < frac:
+            return k
+        l += 1
+
+# 44043947822
 def problem207():
-	perfect = 0
-	notperfect = 0
-	getcontext().prec = 10
-	k = Decimal(1)
-	while True:
-		k += 1
-		if k> 30:
-			break
-		t = f(k)
-		print(2**t)
-		if int(2**t) == 2**t:
-			if int(t) == t:
-				perfect += 1
-			else:
-				notperfect += 1
-		print(k, perfect / (perfect+notperfect))
+    return P(1 / 12345)
 
 '''
-Implement using this!
->>> a.ln()/Decimal(2).ln()
-Decimal('3.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001')
->>> b = a.ln()/Decimal(2).ln()
->>> b
-Decimal('3.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001')
->>> b.quantize(10)
-Decimal('3')
->>> b.quantize(10) == Decimal(3)
-True
-'''
+Split P(n) so that it calls a function to check if perfect or not
+make it return -1 for notperfect, 1 for perfect 0 for nothing
 
+Make a list of values you know are perfect, not perfect, nothing and use tests to check how things changes
+'''
 from cProfile import run
 if __name__ == "__main__":
-	#run("problem207()")
-	print(problem207()) 
+    run("problem207()")
+    print(problem207() == 44043947822)
