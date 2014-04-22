787fd167206aac355cb4230d854971420a06b995
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-04-22 11:18:33 -0700

    fast solution from help

--- Problem138.py
+++ Problem138.py
@@ -1,5 +1,3 @@
-#!/usr/local/bin/python3.3
-
 '''
 http://projecteuler.net/problem=138
 Special isosceles triangles
@@ -8,30 +6,20 @@ Problem 138
 
 '''
 Notes on problem 138():
+Since I always forget how to deal with Pell equations, I went to get some help:
+http://www.mathblog.dk/project-euler-138-special-isosceles-triangles/
 '''
 
-from quadratics import isSquare
-def aSolution(b):
-	for h in [b-1,b+1]:
-		if isSquare( (b//2)**2 + h**2 ):
-			return True
-	return False
-
-from itertools import count
-from PE_primes import factorize
 def problem138():
 	total = 0
-	found = 0
-	for b in count(0,2**4):
-		if aSolution(b):
-			print(b,factorize(b))
-			found += 1
-		if found == 12:
-			break
-
-
+	x = 0
+	y = -1
+	for i in range(0,12):
+	    x, y = -9 * x + -4 * y + 4, -20 * x + -9 * y + 8
+	    total += abs(y)
+	return total
 
 from cProfile import run
 if __name__ == "__main__":
-	#run("problem138()")
-	print(problem138()) 
+	run("problem138()")
+	print(problem138() == 1118049290473932) 
