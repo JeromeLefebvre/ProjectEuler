17cc63607d2aab9d97f3ffaac7211ffd02029a17
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-04-22 11:00:31 -0700

    Fast solution to 164

--- Problem164.py
+++ Problem164.py
@@ -1,5 +1,3 @@
-#!/usr/local/bin/python3.3
-
 '''
 http://projecteuler.net/problem=164
 Numbers for which no three consecutive digits have a sum greater than a given value
@@ -8,20 +6,25 @@ Problem 164
 
 '''
 Notes on problem 164():
+Easy dynamic programing + memoization.
 '''
 
-def problem164():
-	def noThrees(sofar=[]):
-		if len(sofar) <= 0:
-			return sum( noThrees(sofar + [i]) for i in range(1,10))
-		if len(sofar) <= 1:
-			return sum( noThrees(sofar + [i]) for i in range(0,10 - sofar[-1] + 1))
-		if len(sofar) == 10:
-			return 1
-		return sum( noThrees(sofar + [i]) for i in range(0, 9 - sofar[-1] - sofar[-2] + 1))
-	return noThrees()
+from functools import lru_cache
 
+
+def problem164():
+    @lru_cache(maxsize=200)
+    def search(digits, n1, n0):
+        if digits == 20:
+            return 1
+        total = 0
+        for i in range(10 - n0 - n1):
+            total += search(digits + 1, n0, i)
+        return total
+    return sum([search(1, 0, i) for i in range(1, 10)])  # leading digits
+
+# 378158756814587
 from cProfile import run
 if __name__ == "__main__":
-	#run("problem164()")
-	print(problem164()) 
+    # run("problem164()")
+    print(problem164())
